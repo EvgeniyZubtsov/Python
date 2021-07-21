9dfb1452900c375db6decd89745c823221cfbd7e
Evgeniy Zubtsov <ezubtsov@ozon.ru>
2021-07-21 16:36:53 +0300

    Change

--- Homework3/Task3_1.py
+++ Homework3/Task3_1.py
@@ -1,11 +1,8 @@
-import random
-
 string1 = 'Съешь ещё этих мягких французских булок ДА выпей же чаю'
 string2 = string1.split()
 print(string2[3].upper())
 print(string2[6].lower())
 print(string2[7][2])
-words = string1.split()
 for i in string2:
     print(i)
 
